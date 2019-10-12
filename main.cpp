e2a3b12e46beb63f8907e256ed596863a7c994c1
Ché <39664001+goonmax@users.noreply.github.com>
2019-10-12 18:21:59 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,26 +13,6 @@ using namespace std;
 using namespace cv;
 
 
-
-static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';'){
-    std::ifstream file(filename.c_str(), ifstream::in);
-    if(!file){
-        string error = "no input file make no work did a  bad bad !!!";
-    }
-
-    string line, path, classlabel;
-    while(getline(file, line)) {
-        stringstream liness(line);
-        getline(liness, path, separator);
-        getline(liness, classlabel);
-        if(!path.empty() && !classlabel.empty()) {
-            images.push_back(imread(path, 0));
-            labels.push_back(atoi(classlabel.c_str()));
-        }
-    }
-}
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -40,20 +20,15 @@ int main(int argc, char *argv[])
     w.show();
     Mat img;
 
-    VideoCapture cam(0);
-    namedWindow("CameraObjectTrace");
-
-
-    while(1){
-        cam >> img;
-        imshow("CameraObjectTrace", img);
-
-
-        if(waitKey(1) == 27)
-            break;
-    }
 
 
 
     return a.exec();
+}
+
+
+
+void facedetection()
+{
+   
 }
